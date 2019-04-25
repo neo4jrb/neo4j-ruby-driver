ffcaae4d29bc57a6a5c074d7ad28c6ff9685c9e3
lkalwa <leszek.kalwa@gmail.com>
2019-04-25 14:57:58 +0200

    Fixing typo

--- ffi/neo4j/driver/bolt/time.rb
+++ ffi/neo4j/driver/bolt/time.rb
@@ -5,7 +5,7 @@ module Bolt
     extend Bolt::Library
 
     attach_function :get_time, :BoltTime_get_time, %i[pointer], :int
-    attach_function :get_time_ms, :BBoltTime_get_time_ms, [], :int64_t
+    attach_function :get_time_ms, :BoltTime_get_time_ms, [], :int64_t
     attach_function :get_time_ms_from, :BoltTime_get_time_ms_from, %i[pointer], :int64_t
     attach_function :diff_time, :BoltTime_diff_time, %i[pointer pointer pointer], :void
   end
