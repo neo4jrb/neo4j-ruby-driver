7a85eb77c3669ce5578a427d374c83c3f4528a19
Heinrich Klobuczek <heinrich@mail.com>
2019-06-28 23:21:07 +0200

    codeclimate

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'zeitwerk'
 
 class Loader
@@ -5,7 +7,7 @@ class Loader
     loader = Zeitwerk::Loader.new
     loader.tag = 'neo4j-ruby-driver'
     loader.push_dir(File.expand_path(__dir__))
-    loader.push_dir(File.dirname(File.dirname(caller_locations.first.path)))
+    loader.push_dir(File.dirname(File.dirname(caller_locations(1..1).first.path)))
     loader.ignore(File.expand_path('neo4j-ruby-driver_jars.rb', __dir__))
     loader.inflector = Zeitwerk::GemInflector.new(File.expand_path('neo4j/driver', __dir__))
     loader.setup
