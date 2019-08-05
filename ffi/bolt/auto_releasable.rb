87a9f09b4bdf4a387b080eab8a4dd4c2bbd8f75b
lkalwa <leszek.kalwa@gmail.com>
2019-08-05 19:39:27 +0200

    Trying to break dependency deadlock

--- ffi/bolt/auto_releasable.rb
+++ ffi/bolt/auto_releasable.rb
@@ -5,6 +5,8 @@ module Bolt
     extend FFI::Library
     ffi_lib ENV['SEABOLT_LIB']
 
+    puts "env: #{ENV['SEABOLT_LIB']}"
+
     def attach_function(name, func, args, returns = nil, options = nil)
       return super unless returns == :auto_pointer
 
