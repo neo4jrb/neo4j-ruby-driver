3812362e4278236625ad2d1b5e421092b9520da1
lkalwa <leszek.kalwa@gmail.com>
2019-08-05 17:58:36 +0200

    Back to previous state

--- ffi/bolt/auto_releasable.rb
+++ ffi/bolt/auto_releasable.rb
@@ -2,7 +2,6 @@
 
 module Bolt
   module AutoReleasable
-    extend Bolt::Library
 
     def attach_function(name, func, args, returns = nil, options = nil)
       return super unless returns == :auto_pointer
