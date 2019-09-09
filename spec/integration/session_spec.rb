cc8b5ccfd315329a60302291039bec303d20cf83
amitsuryavanshi <amitbsuryavanshi@gmail.com>
2019-09-09 14:42:44 +0530

    Integration specs for session

--- spec/integration/session_spec.rb
+++ spec/integration/session_spec.rb
@@ -113,6 +113,118 @@ RSpec.describe 'SessionSpec' do
     expect(val).to eq(0)
   end
 
+
+  it 'rolls back read transaction when exception is thrown' do
+    session = driver.session
+    expect(session.last_bookmark).to be nil
+    expect { 
+      session.read_transaction do |tx|
+        val = tx.run('RETURN 42').single[0]
+        raise Neo4j::Driver::Exceptions::IllegalStateException if val == 42
+        1
+      end
+    }.to raise_error Neo4j::Driver::Exceptions::IllegalStateException
+    expect(session.last_bookmark).to be nil
+  end
+ 
+  it 'rolls back write transaction when exception is thrown' do
+    session = driver.session
+    expect(session.last_bookmark).to be nil
+    expect {
+      session.write_transaction do |tx|
+        tx.run("CREATE (:Person {name: 'Natasha Romanoff'})")
+        raise Neo4j::Driver::Exceptions::IllegalStateException
+      end
+    }.to raise_error Neo4j::Driver::Exceptions::IllegalStateException
+    val = driver.session do |session|
+      session.run("MATCH (p:Person {name: 'Natasha Romanoff'}) RETURN count(p)").single[0]
+    end
+    expect(val).to eq(0)
+  end
+
+  it 'rolls back read transaction when marked both success and failure' do
+    session = driver.session
+    expect(session.last_bookmark).to be nil
+    answer = session.read_transaction do |tx|
+      val = tx.run('RETURN 42').single[0]
+      tx.success
+      tx.failure
+      val
+    end
+    expect(answer).to eq(42)
+    expect(session.last_bookmark).to be nil
+  end
+
+  it 'rolls back write transaction when marked both success and failure' do
+    session = driver.session
+    expect(session.last_bookmark).to be nil
+    answer = session.write_transaction do |tx|
+      val = tx.run("CREATE (:Person {name: 'Natasha Romanoff'})")
+      tx.success
+      tx.failure
+      42
+    end
+    expect(answer).to eq(42)
+    val = driver.session do |session|
+      session.run("MATCH (p:Person {name: 'Natasha Romanoff'}) RETURN count(p)").single[0]
+    end
+    expect(val).to eq(0)
+  end
+
+
+  it 'rolls back read transaction when marked success and throws exception' do
+    session = driver.session
+    expect(session.last_bookmark).to be nil
+    expect { 
+      session.read_transaction do |tx|
+        tx.run('RETURN 42').single[0]
+        tx.success
+        raise Neo4j::Driver::Exceptions::IllegalStateException
+      end
+    }.to raise_error Neo4j::Driver::Exceptions::IllegalStateException
+    expect(session.last_bookmark).to be nil
+  end
+
+  it 'rolls back write transaction when marked success and exception is thrown' do
+    session = driver.session
+    expect(session.last_bookmark).to be nil
+    expect {
+      session.write_transaction do |tx|
+        tx.run("CREATE (:Person {name: 'Natasha Romanoff'})")
+        tx.success
+        raise Neo4j::Driver::Exceptions::IllegalStateException
+      end
+    }.to raise_error Neo4j::Driver::Exceptions::IllegalStateException
+    val = driver.session do |session|
+      session.run("MATCH (p:Person {name: 'Natasha Romanoff'}) RETURN count(p)").single[0]
+    end
+    expect(val).to eq(0)
+  end
+
+  # it 'transactionRunShouldFailOnDeadlocks' do
+
+  # end
+
+  # it 'writeTransactionFunctionShouldRetryDeadlocks' do
+
+  # end
+
+  # it 'shouldExecuteTransactionWorkInCallerThread' do
+
+  # end
+
+  it 'should propagate failure when closed' do
+    session = driver.session
+    session.run('RETURN 10 / 0')
+    expect { session.close }.to raise_error Neo4j::Driver::Exceptions::ClientException, '/ by zero'
+  end
+
+  it 'should Propagate Pull All Failure When Closed' do
+    session = driver.session
+    session.run('UNWIND range(20000, 0, -1) AS x RETURN 10 / x')
+    expect { session.close }.to raise_error Neo4j::Driver::Exceptions::ClientException, '/ by zero'
+  end
+
   def test_read_transaction(mode)
     driver.session do |session|
       session.run("CREATE (:Person {name: 'Tony Stark'})").consume
