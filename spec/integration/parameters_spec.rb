ea7691b57aee9c14ce605acdcc8d63eff8e5d9a5
amitsuryavanshi <amitbsuryavanshi@gmail.com>
2020-01-20 18:48:53 +0530

    spec correction

--- spec/integration/parameters_spec.rb
+++ spec/integration/parameters_spec.rb
@@ -27,8 +27,8 @@ RSpec.describe 'Parameters' do
 
     it 'is able to set and return Date property' do
       test_property Date.today
-      result = session.run('match(n) return n.value').single
-      expect(result.as_map['n.value']).to be_a(Date) if result.respond_to?(:as_map)
+      result = session.run('match(n) return n').single
+      expect(result[:n].properties[:value]).to be_a(Date)
     end
     # skiped, no such types in ruby
     # shouldBeAbleToSetAndReturnByteProperty
