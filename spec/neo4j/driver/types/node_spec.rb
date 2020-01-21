344242be146907ddc05a7c0a3e8bc732bfd8e1da
amitsuryavanshi <amitbsuryavanshi@gmail.com>
2020-01-21 23:11:17 +0530

    spec refactor

--- spec/neo4j/driver/types/node_spec.rb
+++ spec/neo4j/driver/types/node_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe Neo4j::Driver::Types::Node do
   subject do
     session = driver.session
     session.write_transaction do |tx|
-      tx.run('CREATE (p:Person{name: "John", created: $date}) RETURN p', date: Date.today).single.first
+      tx.run('CREATE (p:Person{name: "John", created: $date}) RETURN p', date: date).single.first
     end
   ensure
     session&.close
   end
-
+  let(:date) { Date.today }
   it { is_expected.to be_a_kind_of described_class }
   its(:labels) { is_expected.to eq(%i[Person]) }
   its(:id) { is_expected.to be_a(Integer) }
-  its(:properties) { is_expected.to eq(name: 'John', created: Date.today) }
+  its(:properties) { is_expected.to eq(name: 'John', created: date) }
 end
