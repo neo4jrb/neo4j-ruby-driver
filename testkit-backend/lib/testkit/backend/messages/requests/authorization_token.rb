41f6e8c31171f4c7850d4cdbc591f78661b3591e
Heinrich Klobuczek <heinrich@mail.com>
2021-02-15 18:08:10 -0900

    testkit passes

--- testkit-backend/lib/testkit/backend/messages/requests/authorization_token.rb
+++ testkit-backend/lib/testkit/backend/messages/requests/authorization_token.rb
@@ -2,7 +2,7 @@ module Testkit::Backend::Messages
   module Requests
     class AuthorizationToken < Request
       def to_object
-        Neo4j::Driver::AuthTokens.send(scheme, principal, credentials)
+        Neo4j::Driver::AuthTokens.send(scheme, principal, credentials, realm)
       end
     end
   end
