b147fe6ba9223e9b7dcdbadd5186aee379ae7fdd
Heinrich Klobuczek <heinrich@mail.com>
2021-10-22 19:30:32 +0200

    routing via resolver

--- spec/neo4j/driver/util/cc/cluster_member.rb
+++ spec/neo4j/driver/util/cc/cluster_member.rb
@@ -9,10 +9,11 @@ module Neo4j
           #ROUTING_SCHEME = 'bolt+routing://'
           ROUTING_SCHEME = 'neo4j://'
 
-          attr_accessor :bolt_uri, :path
+          attr_accessor :bolt_uri, :path, :bolt_address
 
           def initialize(bolt_uri, path)
             self.bolt_uri = bolt_uri
+            self.bolt_address = Neo4j::Driver::Net::ServerAddress.of(*URI(bolt_uri).then { |uri| [uri.host, uri.port] })
             self.path = path
           end
 
