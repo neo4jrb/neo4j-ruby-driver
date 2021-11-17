74f8ecbe5be93e91826f8eb01ed1aed9633e8f90
Hardik Joshi <hardikjoshi1991@gmail.com>
2021-11-17 16:49:01 +0530

    changed order of constant

--- ruby/neo4j/driver/config.rb
+++ ruby/neo4j/driver/config.rb
@@ -5,25 +5,6 @@ module Neo4j
     class Config < Hash
       include Ext::ConfigConverter
 
-      DEFAULTS = {
-        connection_acquisition_timeout: 1.minute, #:set_max_connection_acquisition_time
-        connection_timeout: 30.seconds, # BoltSocketOptions_set_connect_timeout
-        encryption: false, # :set_transport
-        fetch_size: 1000,
-        idle_time_before_connection_test: -1,
-        keep_alive: true, # BoltSocketOptions_set_keep_alive
-        logger: ActiveSupport::Logger.new(STDOUT, level: ::Logger::ERROR), # :set_log
-        leaked_session_logging: false,
-        #connection_liveness_check_timeout: -1, # Not configured
-        max_connection_lifetime: 1.hour, # :set_max_connection_life_time
-        max_connection_pool_size: 100, #:set_max_pool_size
-        max_transaction_retry_time: Internal::Retry::ExponentialBackoffRetryLogic::DEFAULT_MAX_RETRY_TIME,
-        metrics_enabled: false,
-        # resolver: nil # :set_address_resolver
-        trust_strategy: Neo4j::Driver::Config::TrustStrategy.trust_all_certificates,
-        user_agent: "neo4j-java/#{Neo4j::Driver::VERSION}".freeze
-      }
-
       class TrustStrategy
         TRUST_ALL_CERTIFICATES = :trust_all_certificates
         TRUST_CUSTOM_CA_SIGNED_CERTIFICATES = :trust_custom_ca_signed_certificates
@@ -54,6 +35,25 @@ module Neo4j
           java.io.File.new(cert_file.path)
         end
       end
+
+      DEFAULTS = {
+        connection_acquisition_timeout: 1.minute, #:set_max_connection_acquisition_time
+        connection_timeout: 30.seconds, # BoltSocketOptions_set_connect_timeout
+        encryption: false, # :set_transport
+        fetch_size: 1000,
+        idle_time_before_connection_test: -1,
+        keep_alive: true, # BoltSocketOptions_set_keep_alive
+        logger: ActiveSupport::Logger.new(STDOUT, level: ::Logger::ERROR), # :set_log
+        leaked_session_logging: false,
+        #connection_liveness_check_timeout: -1, # Not configured
+        max_connection_lifetime: 1.hour, # :set_max_connection_life_time
+        max_connection_pool_size: 100, #:set_max_pool_size
+        max_transaction_retry_time: Internal::Retry::ExponentialBackoffRetryLogic::DEFAULT_MAX_RETRY_TIME,
+        metrics_enabled: false,
+        # resolver: nil # :set_address_resolver
+        trust_strategy: TrustStrategy.trust_all_certificates,
+        user_agent: "neo4j-java/#{Neo4j::Driver::VERSION}".freeze
+      }
 
       def initialize(**config)
         init_security_and_trust_config(config)
