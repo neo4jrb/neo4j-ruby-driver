978d29e9bfdff2206b9c47a30feac4072916a215
Kinjal Patel <kinju011@gmail.com>
2021-12-03 22:13:18 +0530

    internal async connection files conversion correction made and comments are added

--- ruby/neo4j/driver/internal/async/connection/channel_attributes.rb
+++ ruby/neo4j/driver/internal/async/connection/channel_attributes.rb
@@ -1,3 +1,21 @@
+
+# Copyright (c) "Neo4j"
+# Neo4j Sweden AB [http://neo4j.com]
+
+# This file is part of Neo4j.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 module Neo4j::Driver
   module Internal
     module Async
@@ -125,7 +143,7 @@ module Neo4j::Driver
             def set_once(channel, key, value)
               existing_value = channel.attr(key).set_if_absent(value)
 
-              if !existing_value.nil?
+              unless existing_value.nil?
                 raise Neo4j::Driver::Exceptions::CertificateException::IllegalStateException, "Unable to set #{key.name} because it is already set to #{existing_value}"
               end
             end
