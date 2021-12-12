d5171133c718b68da4424d4d7181d37c4f1f93ea
Heinrich Klobuczek <heinrich@mail.com>
2021-12-12 15:49:23 +0100

    more delegation

--- ruby/neo4j/driver/internal/async/internal_async_transaction.rb
+++ ruby/neo4j/driver/internal/async/internal_async_transaction.rb
@@ -2,15 +2,10 @@ module Neo4j::Driver
   module Internal
     module Async
       class InternalAsyncTransaction
+        delegate :run_async, :commit_async, :rollback_async, :open?, to: :@tx
 
         def initialize(tx)
           @tx = tx
-        end
-
-        delegate :commit_async, :rollback_async, :open?, to: :@tx
-
-        def run_async(query)
-          @tx.run_async(query)
         end
       end
     end
