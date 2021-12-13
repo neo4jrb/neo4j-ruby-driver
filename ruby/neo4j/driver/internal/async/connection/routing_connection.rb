8e07f6f88cf8f24ee0f68dbf169f82febe3d4d43
Heinrich Klobuczek <heinrich@mail.com>
2021-12-13 01:29:03 +0100

    simplification of Connection

--- ruby/neo4j/driver/internal/async/connection/routing_connection.rb
+++ ruby/neo4j/driver/internal/async/connection/routing_connection.rb
@@ -2,17 +2,11 @@ module Neo4j::Driver
   module Internal
     module Async
       module Connection
-
         # A connection used by the routing driver.
         class RoutingConnection < Struct.new(:delegate, :database_name, :access_mode, :impersonated_user, :error_handler)
-
-          def enable_auto_read
-            delegate.enable_auto_read
-          end
-
-          def disable_auto_read
-            delegate.disable_auto_read
-          end
+          delegate *%i[enable_auto_read disable_auto_read reset open? release terminate_and_release server_agent
+          server_address server_version protocol flush], to: :delegate
+          alias mode access_mode
 
           def write(message1, handler1, message2 = nil, handler2 = nil)
             if message2.present? && handler2.present?
@@ -28,54 +22,6 @@ module Neo4j::Driver
             else
               delegate.write_and_flush(message1, handler1)
             end
-          end
-
-          def reset
-            delegate.reset
-          end
-
-          def is_open?
-            delegate.is_open?
-          end
-
-          def release
-            delegate.release
-          end
-
-          def terminate_and_release(reason)
-            delegate.terminate_and_release(reason)
-          end
-
-          def server_agent
-            delegate.server_agent
-          end
-
-          def server_address
-            delegate.server_address
-          end
-
-          def server_version
-            delegate.server_version
-          end
-
-          def protocol
-            delegate.protocol
-          end
-
-          def flush
-            delegate.flush
-          end
-
-          def mode
-            mode
-          end
-
-          def database_name
-            database_name
-          end
-
-          def impersonated_user
-            impersonated_user
           end
 
           private
