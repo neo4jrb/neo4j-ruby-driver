6042201fc5459948f3de24f632f71f26670e89d1
Kinjal Patel <kinju011@gmail.com>
2021-12-13 11:38:00 +0530

    latest changes included

--- ruby/neo4j/driver/internal/async/internal_async_transaction.rb
+++ ruby/neo4j/driver/internal/async/internal_async_transaction.rb
@@ -2,12 +2,11 @@ module Neo4j::Driver
   module Internal
     module Async
       class InternalAsyncTransaction
+        delegate :run_async, :commit_async, :rollback_async, :open?, to: :@tx
 
         def initialize(tx)
           @tx = tx
         end
-
-        delegate :commit_async, :rollback_async, :open?, :run_async, to: :@tx
       end
     end
   end
