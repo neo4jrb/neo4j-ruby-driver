fc1f553c3dcfa133db8047835566f42699716908
Kinjal Patel <kinju011@gmail.com>
2022-01-14 18:56:30 +0530

    iterables migrated

--- ruby/neo4j/driver/internal/util/iterables.rb
+++ ruby/neo4j/driver/internal/util/iterables.rb
@@ -2,10 +2,31 @@ module Neo4j::Driver
   module Internal
     module Util
       class Iterables
+        EMPTY_QUEUE = Queue.new
 
         class << self
-          def count
+          def single(it)
+            if it.empty?
+              raise ArgumentError, 'Given iterable is empty'
+            end
 
+            result = it.first
+
+            if it.size > 1
+              raise ArgumentError, "Given iterable contains more than one element: #{it}"
+            end
+
+            result
+          end
+
+          def map(alternating_key_value)
+            out = {}
+
+            (0...alternating_key_value.length).step(2) do |i|
+              out[i] = out[i+1]
+            end
+
+            out
           end
         end
       end
