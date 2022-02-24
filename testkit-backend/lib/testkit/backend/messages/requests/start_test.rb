73811a25341d4ca5c5436d4fbf5a9e2c65fa45ab
Kinjal Patel <kinju011@gmail.com>
2022-02-24 18:20:34 +0530

    running back end incomplete tests

--- testkit-backend/lib/testkit/backend/messages/requests/start_test.rb
+++ testkit-backend/lib/testkit/backend/messages/requests/start_test.rb
@@ -84,13 +84,15 @@ module Testkit::Backend::Messages
         elsif reason = SKIPPED_PATTERN.find { |expr, _| testName.match?(expr) }&.last
           skip(reason)
         elsif BACKEND_INCOMPLETE.any?(&testName.method(:match?))
-          skip('Backend Incomplete')
+          # skip('Backend Incomplete')
+          run
         elsif RUBY_DRIVER_PROBLEMS.include?(testName)
           skip('ruby driver problem')
         elsif ENV['driver'] == 'java' && DOMAIN_RESOLVER_ON_JAVA.any?(&testName.method(:match?))
           skip('Domain Resolver hard to implement on jruby due to default visibility and protected not implemented correctly in jruby')
         else
-          run
+          # run
+          skip('skipped')
         end
       end
 
