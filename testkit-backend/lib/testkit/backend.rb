5bd768f74610f9a6f905d11e18b33192b820aeae
Heinrich Klobuczek <heinrich@mail.com>
2022-04-30 00:12:11 +0200

    added nio dependency for testkit-backend

--- testkit-backend/lib/testkit/backend.rb
+++ testkit-backend/lib/testkit/backend.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/module/attribute_accessors"
 require 'active_support/inflector'
 require 'async/io'
 require 'neo4j_ruby_driver'
+require 'nio'
 require 'testkit/backend/loader'
 
 Testkit::Backend::Loader.load
