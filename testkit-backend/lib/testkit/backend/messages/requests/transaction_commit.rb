33a5d33e6a6e69acb901c8828241b2da2eaedc25
Kinjal Patel <kinju011@gmail.com>
2022-05-05 21:43:56 +0530

    changes in transaction commit request and response

--- testkit-backend/lib/testkit/backend/messages/requests/transaction_commit.rb
+++ testkit-backend/lib/testkit/backend/messages/requests/transaction_commit.rb
@@ -1,12 +1,8 @@
 module Testkit::Backend::Messages
   module Requests
     class TransactionCommit < Request
-      def process
-        reference('Transaction')
-      end
-
-      def to_object
-        fetch(txId).tap(&:commit)
+      def response
+        Responses::Result.new(fetch(txId).tap(&:commit))
       end
     end
   end
