c3db4154f41d17d17410dd86ee72eda879a23a95
Kinjal Patel <kinju011@gmail.com>
2022-05-05 21:46:05 +0530

    transaction rollback response changed

--- testkit-backend/lib/testkit/backend/messages/requests/transaction_rollback.rb
+++ testkit-backend/lib/testkit/backend/messages/requests/transaction_rollback.rb
@@ -1,12 +1,8 @@
 module Testkit::Backend::Messages
   module Requests
     class TransactionRollback < Request
-      def process
-        reference('Transaction')
-      end
-
-      def to_object
-        fetch(txId).tap(&:rollback)
+      def response
+        Responses::Result.new(fetch(txId).tap(&:rollback))
       end
     end
   end
