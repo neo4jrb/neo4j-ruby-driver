5115bd868f65c33e7a26785af8aed17615142648
Kinjal Patel <kinju011@gmail.com>
2022-05-05 16:42:58 +0530

    resolver_resolution request implemented

--- testkit-backend/lib/testkit/backend/messages/requests/resolver_resolution_completed.rb
+++ testkit-backend/lib/testkit/backend/messages/requests/resolver_resolution_completed.rb
@@ -2,6 +2,11 @@ module Testkit::Backend::Messages
   module Requests
     class ResolverResolutionCompleted < Request
       def process
+        named_entity('ResolverResolutionRequired', id: requestId, address: address)
+      end
+
+      def to_object
+        fetch(requestId).resolver(addresses: @command_processor.process(blocking: true).addresses)
       end
     end
   end
