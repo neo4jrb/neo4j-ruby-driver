a3269acc5448f37241b7bb98cbdff40ef415add2
Kinjalkumar Patel <kinju011@gmail.com>
2022-09-02 20:09:09 +0530

    testing failing tests

--- testkit-backend/lib/testkit/backend/messages/responses/driver_error.rb
+++ testkit-backend/lib/testkit/backend/messages/responses/driver_error.rb
@@ -2,6 +2,15 @@ module Testkit::Backend::Messages
   module Responses
     class DriverError < Response
       def data
+        p 111111111
+        p @object.class.name
+        p 3333333333333333333333333333333
+        p @object.message
+        p 4444444444444444444444444444444
+        p @object.code
+        p 4444444444444444444444444444444
+        p @object.methods
+        p 111111111
         { id: store(@object), errorType: @object.class.name, msg: @object.message, code: @object.try(:code) }.compact
       end
     end
