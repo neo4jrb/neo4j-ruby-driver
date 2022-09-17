0d679a5fe839ed025171a2fb1d5811e4c7176fec
Kinjal Patel <kinju011@gmail.com>
2022-09-17 18:40:08 +0530

    fixed the example spec with neo4j scheme (#198)
    * fixed the example spec with neo4j scheme
    
    * made requested changes
    
    * spec updated to support older version
    
    * corrected spec to test what it is supposed to test
    
    * moved resolver spec to cluster tests
    
    Co-authored-by: Heinrich Klobuczek <heinrich@mail.com>

--- spec/neo4j/driver/dev_manual_examples_spec.rb
+++ spec/neo4j/driver/dev_manual_examples_spec.rb
@@ -32,18 +32,6 @@ RSpec.describe Neo4j::Driver do
       it { is_expected.to be true }
     end
 
-    context 'Example 2.3. Custom Address Resolver' do
-      let(:config) do
-        { resolver: lambda {
-          [Neo4j::Driver::Net::ServerAddress.of('a.acme.com', 7676),
-           Neo4j::Driver::Net::ServerAddress.of('b.acme.com', 8787),
-           Neo4j::Driver::Net::ServerAddress.of('c.acme.com', 9898)]
-        } }
-      end
-
-      it { is_expected.to be true }
-    end
-
     context 'No authentication', auth: :none do
       let(:auth_tokens) { Neo4j::Driver::AuthTokens.none }
 
@@ -73,7 +61,7 @@ RSpec.describe Neo4j::Driver do
     end
 
     context 'Example 2.8. Trust' do
-      let(:config) { { trust_strategy: {strategy: :trust_all_certificates} } }
+      let(:config) { { trust_strategy: { strategy: :trust_all_certificates } } }
 
       it { is_expected.to be true }
     end
