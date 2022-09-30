e539ca4216b076b7a4806d39e22949c2befa1126
Heinrich Klobuczek <heinrich@mail.com>
2022-10-01 00:16:47 +0200

    enabled some features

--- testkit-backend/lib/testkit/backend/messages/requests/get_features.rb
+++ testkit-backend/lib/testkit/backend/messages/requests/get_features.rb
@@ -6,23 +6,23 @@ module Testkit::Backend::Messages
           'Feature:API:ConnectionAcquisitionTimeout' => '',
           'Feature:API:Driver.IsEncrypted' => 'jr',
           'Feature:API:Liveness.Check' => 'j',
-          'Feature:API:Result.List' => 'jr',
-          'Feature:API:Result.Peek' => 'jr',
+          'Feature:API:Result.List' => 'j',
+          'Feature:API:Result.Peek' => 'j',
           'Feature:API:Result.Single' => 'j',
           'Feature:API:SessionConnectionTimeout' => '',
           'Feature:API:SSLConfig' => 'j',
           'Feature:API:SSLSchemes' => 'j',
-          'Feature:API:Type.Temporal' => 'r',
+          'Feature:API:Type.Temporal' => '',
           'Feature:API:UpdateRoutingTableTimeout' => '',
-          'Feature:Auth:Bearer' => 'j',
-          'Feature:Auth:Custom' => 'j',
-          'Feature:Auth:Kerberos' => 'j',
-          'Feature:Bolt:3.0' => 'jr',
+          'Feature:Auth:Bearer' => 'jr',
+          'Feature:Auth:Custom' => 'jr',
+          'Feature:Auth:Kerberos' => 'jr',
+          'Feature:Bolt:3.0' => 'j',
           'Feature:Bolt:4.0' => 'jr',
-          'Feature:Bolt:4.1' => 'jr',
+          'Feature:Bolt:4.1' => 'j',
           'Feature:Bolt:4.2' => 'jr',
-          'Feature:Bolt:4.3' => 'jr',
-          'Feature:Bolt:4.4' => 'jr',
+          'Feature:Bolt:4.3' => 'j',
+          'Feature:Bolt:4.4' => 'j',
           'Feature:Bolt:Patch:UTC' => 'j',
           'Feature:Impersonation' => 'j',
           'Feature:TLS:1.1' => '', # TODO works for java,
@@ -40,12 +40,12 @@ module Testkit::Backend::Messages
           'Detail:ResultStreamWorksAfterBrokenRecord' => '',
           'ConfHint:connection.recv_timeout_seconds' => 'j',
         }
-    end
 
-    def process
-      platform = RUBY_PLATFORM == 'java' ? 'j' : 'r'
-      features_list = FEATURES.select { |_, value| value.include?(platform) }.keys
-      named_entity('FeatureList', features: features_list)
+      def process
+        platform = RUBY_PLATFORM == 'java' ? 'j' : 'r'
+        features_list = FEATURES.select { |_, value| value.include?(platform) }.keys
+        named_entity('FeatureList', features: features_list)
+      end
     end
   end
 end
