dee49b0441588f7decf6ec988d7e9f301cb361eb
Heinrich Klobuczek <heinrich@mail.com>
2023-10-22 20:26:39 +0200

    core_ext blank?

--- lib/neo4j-ruby-driver_loader.rb
+++ lib/neo4j-ruby-driver_loader.rb
@@ -2,6 +2,7 @@
 
 require 'active_support/concern'
 require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/core_ext/object/blank'
 require 'active_support/isolated_execution_state' if Gem::Requirement.create('>= 7').satisfied_by?(Gem.loaded_specs["activesupport"].version) # TODO: this should not be necessary https://github.com/rails/rails/issues/43851
 require 'active_support/deprecator' if Gem::Requirement.create('>= 7.1').satisfied_by?(Gem.loaded_specs["activesupport"].version)
 require 'active_support/deprecation'
