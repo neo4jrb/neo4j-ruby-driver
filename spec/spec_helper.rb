91243f6ead8c53aedf2c744493042220eb7b66fe
Piotr Gębala <piotrek.gebala@gmail.com>
2025-03-18 13:40:44 +0100

    Add pry dev dependency for debugging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ require 'ffaker'
 require 'rspec/its'
 require 'support/driver_helper'
 require 'support/neo4j_cleaner'
+require 'pry'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
