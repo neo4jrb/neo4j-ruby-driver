ed76a8133260bf9169e892f3488114c8af19d3d0
Nick Saukin <nick@sauk.in>
2025-06-06 15:23:25 +0200

    Fix testkit request (WIP)

--- testkit-backend/lib/testkit/backend/messages/requests/execute_query.rb
+++ testkit-backend/lib/testkit/backend/messages/requests/execute_query.rb
@@ -1,16 +1,8 @@
-# frozen_string_literal: true
-
 module Testkit::Backend::Messages
   module Requests
     class ExecuteQuery < Request
       def response
-        Responses::Result.new(fetch(driver_id).execute_query(cypher, auth_token, config, decode(params)))
-      end
-
-      private
-
-      def auth_token
-        config.authorizationToken
+        Responses::EagerResult.new(fetch(driver_id).execute_query(cypher, auth_token, config, **decode(params)))
       end
     end
   end
