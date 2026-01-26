b2fe738473d4890b4d20f4fc2f309ee4f7d727a0
Nick Saukin <nick@sauk.in>
2026-01-26 21:08:28 +0100

    Fix TimedStack initialization (#267)
    * Fix TimedStack initialization
    
    * Fix pop call
    
    * Fix argument key for pop call
    
    * Put lower bound on connection_pool version
    
    * Bump version to 6.0.0.alpha.1
    
    * Update specs workflow to include 6.x branch
    
    ---------
    
    Co-authored-by: klobuczek <heinrich@mail.com>

--- ruby/neo4j/driver/internal/async/pool/channel_pool.rb
+++ ruby/neo4j/driver/internal/async/pool/channel_pool.rb
@@ -5,11 +5,11 @@ module Neo4j::Driver
         class ChannelPool < ConnectionPool
           def initialize(limit: nil, acquisition_timeout: nil, &block)
             super(size: limit, timeout: acquisition_timeout, &block)
-            @available = TimedStack.new(@size, &block)
+            @available = TimedStack.new(size: @size, &block)
           end
 
           def acquire(options = {})
-            @available.pop(options[:timeout] || @timeout)
+            @available.pop(timeout: options[:timeout] || @timeout)
           end
 
           def release(resource)
